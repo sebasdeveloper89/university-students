cf7d4e704a1516b5a3c6a6f2bbe43561943c6c88
Sebastian Cardona <sebasdeveloper89@gmail.com>
2021-10-03 11:10:36 -0500

    Se crea el proyecto de consola

---
+++ University.Students.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Students.Domain;
+
+namespace University.Students.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oStudent = new Teacher();
+            oStudent.FirstName = "Sebastian";
+            oStudent.LastName = "Cardona";
+
+            var fullName = oStudent.ShowFullName();
+
+            System.Console.WriteLine(fullName);
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ University.Students.Domain/Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Students.Domain
+{
+    public class Team
+    {
+        public int IdTeam { get; set; }
+
+        public string IdentificationNumber { get; set; }
+
+        public string IdentificationType {get;set;}
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string ShowFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
