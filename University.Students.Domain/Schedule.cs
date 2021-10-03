e35a153be7bd0175e87ee812b1c0702abece7bb8
Sebastian Cardona <sebasdeveloper89@gmail.com>
2021-10-03 11:59:35 -0500

    Se crea la clase Schedule

---
+++ University.Students.Domain/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Students.Domain
+{
+    public class Schedule
+    {
+        public int IdSchedule { get; set; }
+        public string Name { get; set; }
+        public string WorkingDay { get; set; }
+    }
+}
